951d24a95b56ec4a21fba608bb7ad15517312416
Malik-Essa99 <malikhodrob99@gmail.com>
2023-06-12 20:08:33 +0000

    removed file

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -37,5 +37,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(message.encode())
+        self.wfile.write(message.encode('utf-8'))
         return
