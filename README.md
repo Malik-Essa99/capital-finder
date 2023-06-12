21e214606ab2d2ffbca0fa73865c67a101d9ce06
Malik-Essa99 <malikhodrob99@gmail.com>
2023-06-12 18:47:10 +0000

    added functionality to capital-finder

---
+++ README.md
@@ -0,0 +1,9 @@
+# lab 16
+### project: Serverless Functions
+### Author: Malik Al Hudrub
+## How to initialize/run your application:
+### python api/capital-finder.py
+## Testing 
+### How do you run tests?
+using this link:
+### Test Notes: None
---
+++ api/capital-finder.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_list = parse.parse_qsl(url_components.query)
+        my_dict = dict(query_list)
+        print(my_dict)
+        if 'country' in my_dict:
+            country = my_dict.get('country')
+            country_url = f"https://restcountries.com/v3.1/name/{country}"
+
+            params = {'country': country}
+            res = requests.get(country_url, params=params)
+            data = res.json()
+
+            for country_info in data:
+                capital = country_info["capital"][0]
+                message = f"The capital of {country} is {capital}"
+                break
+
+        if 'capital' in my_dict:
+            capital = my_dict.get('capital')
+            capital_url = f"https://restcountries.com/v3.1/capital/{capital}"
+
+            params = {'capital': capital}
+            res = requests.get(capital_url, params = params)
+            data = res.json()
+
+            for capital_info in data:
+                country = capital_info["name"]["common"]
+                message = f"{capital} is the capital of {country}"
+                break
+
+        self.wfile.write(message.encode())
+        return
